3ae2ebee45f64aeb5770105483e86b1cd18981a5
legitpotato <40745034+legitpotato@users.noreply.github.com>
2018-07-01 20:11:34 -0400

    Delete ListaDoblementeEnlazada.py

--- .gitignore/ListaDoblementeEnlazada.py
+++
@@ -1,9 +0,0 @@
-class Contacto:
-    def __init__(self,nombre,apellido,telefono,email):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.telefono = telefono
-        self.email = email
-        self.busqueda = nombre+apellido
-        self.next = None
-        self.prev = None
